9377c671447d2ca49c6d5ae9dec38ef6ed832129
Arrian <arrian.mor@gmail.com>
2026-09-09 14:16:07 -0400

    step 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+void main() {
+    String[] classmates = {"Abdullahi Abdullahi", "Jacob Cruz", "MAGAN FARAH", "GARETH FLANDRO", "Leonardo Glorioso", "Muhtasim Haq","ALI INDIA", "Colette Kennedy", "FRANCO MENDOZA", "Ilyas Mohamed", "Satyam Nandi", "Hano Roughley", "ALBERT ZHANG"
+    };
+
+    for (int i =0; i > 14; i++){
+        System.out.println(classmates[i]);
+    }
+}
