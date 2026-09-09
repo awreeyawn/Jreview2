07ca86ff545cb8268c00ec98f31cdfc814aa1dd8
Arrian <arrian.mor@gmail.com>
2026-09-09 14:20:51 -0400

    bugfix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 void main() {
-    String[] classmates = {"Abdullahi Abdullahi", "Jacob Cruz", "MAGAN FARAH", "GARETH FLANDRO", "Leonardo Glorioso", "Muhtasim Haq","ALI INDIA", "Colette Kennedy", "FRANCO MENDOZA", "Ilyas Mohamed", "Satyam Nandi", "Hano Roughley", "ALBERT ZHANG"
+    String[] classmates = {"Abdullahi Abdullahi", "Jacob Cruz", "MAGAN FARAH", "GARETH FLANDRO", "Leonardo Glorioso", "Muhtasim Haq","ALI INDIA", "Colette Kennedy", "FRANCO MENDOZA", "Ilyas Mohamed", "Satyam Nandi", "Hano Roughley", "ALBERT ZHANG", "ARRIAN MORSHED"
     };
 
-    for (int i =0; i > 14; i++){
+    for (int i =0; i < 14; i++){
         System.out.println(classmates[i]);
     }
 }
